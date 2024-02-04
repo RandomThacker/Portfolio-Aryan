f2de11d6dc654276b7177aa1a02ef1d1ecb6ad83
RandomThacker <randomthacker@gmail.com>
2024-02-04 20:55:11 +0530

    added canvas

--- js/script.js
+++ js/script.js
@@ -79,9 +79,10 @@ function heroCursor() {
     el.addEventListener("mouseover", function () {
       gsap.to(".cursorHero", {
         transform: "translate(-50%,-50%) scale(0.7)",
-        backgroundColor: "rgba(22, 14, 14, 0.468)",
+        backgroundColor: "#d1483657",
         border: "2px solid red",
-        innerHTML: `<h1></h1>`,
+        innerHTML: `<h1>View</h1>`,
+        color:"antiquewhite"
       });
     });
   })
@@ -330,6 +331,7 @@ function horizintalScroll() {
     scrollTrigger: {
       trigger: section_2,
       pin: true,
+      markers: true,
       scrub: 1,
       snap: 1 / (box_items.length - 1),
       end: "+=" + section_2.offsetWidth
@@ -363,4 +365,245 @@ function page5ColorChange() {
 }
 page5ColorChange();
 
+
+function dom() {
+  const canvas = document.querySelector(".page5>canvas");
+  const context = canvas.getContext("2d");
+  const imageSeq = {
+    frame: 0,
+  };
+
+  function setCanvasSize() {
+    const img = images[imageSeq.frame];
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerWidth;
+    render();
+  }
+
+  window.addEventListener("resize", setCanvasSize);
+
+  function files(index) {
+    const data = `
+      ./img/SequenceImages/jlsousa tires project 4  Urcola+.72.${
+        11 + index
+      }-min.png
+      `;
+    return data.trim();
+  }
+
+  const frameCount = 141;
+  const images = [];
+
+  for (let i = 0; i < frameCount; i++) {
+    const img = new Image();
+    img.src = files(i);
+    images.push(img);
+  }
+
+  gsap.to(imageSeq, {
+    frame: frameCount - 1,
+    snap: "frame",
+    ease: "none",
+    scrollTrigger: {
+      scrub: 1,
+      // pin: true,
+      trigger: ".page5",
+      start: "top center",
+      end: "center top",
+      // markers: true,
+    },
+    onUpdate: render,
+  });
+
+  gsap.to(imageSeq, {
+    scrollTrigger: {
+      scrub: 1,
+      pin: true,
+      trigger: ".page5",
+      start: "top top",
+      end: "center top",
+      // markers: true,
+    },
+  });
+
+  images[0].onload = setCanvasSize;
+
+  function render() {
+    scaleImage(images[imageSeq.frame], context);
+  }
+
+  function scaleImage(img, ctx) {
+    const canvas = ctx.canvas;
+    const hRatio = canvas.width / img.width;
+    const vRatio = canvas.height / img.height;
+    const ratio = Math.max(hRatio, vRatio);
+    const centerShift_x = (canvas.width - img.width * ratio) / 2;
+    const centerShift_y = (canvas.height - img.height * ratio) / 2;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.drawImage(
+      img,
+      0,
+      0,
+      img.width,
+      img.height,
+      centerShift_x,
+      centerShift_y,
+      img.width * ratio,
+      img.height * ratio
+    );
+  }
+}
+
+dom();
+
+
+
+function horscrolltext(){
+
+const scrollingText = gsap.utils.toArray(".rail h4");
+
+const tl = horizontalLoop(scrollingText, {
+  repeat: -1,
+  paddingRight: 20,
+  speed: 3
+});
+
+// Default direction is scrolling down
+let direction = 1;
+let t;
+
+ScrollTrigger.create({
+  trigger: ".content",
+  start: "top top",
+  end: "bottom top",
+  onUpdate: (self) => {
+    if (self.direction !== direction) {
+      direction = self.direction;
+      t && t.kill();
+      t = gsap.to(tl, {
+        duration: 0.3,
+        timeScale: self.direction
+      });
+    }
+  },
+  markers: true
+});
+
+/*
+This helper function makes a group of elements animate along the x-axis in a seamless, responsive loop.
+
+Features:
+ - Uses xPercent so that even if the widths change (like if the window gets resized), it should still work in most cases.
+ - When each item animates to the left or right enough, it will loop back to the other side
+ - Optionally pass in a config object with values like "speed" (default: 1, which travels at roughly 100 pixels per second), paused (boolean),  repeat, reversed, and paddingRight.
+ - The returned timeline will have the following methods added to it:
+   - next() - animates to the next element using a timeline.tweenTo() which it returns. You can pass in a vars object to control duration, easing, etc.
+   - previous() - animates to the previous element using a timeline.tweenTo() which it returns. You can pass in a vars object to control duration, easing, etc.
+   - toIndex() - pass in a zero-based index value of the element that it should animate to, and optionally pass in a vars object to control duration, easing, etc. Always goes in the shortest direction
+   - current() - returns the current index (if an animation is in-progress, it reflects the final index)
+   - times - an Array of the times on the timeline where each element hits the "starting" spot. There's also a label added accordingly, so "label1" is when the 2nd element reaches the start.
+ */
+function horizontalLoop(items, config) {
+  items = gsap.utils.toArray(items);
+  config = config || {};
+  let tl = gsap.timeline({
+      repeat: config.repeat,
+      paused: config.paused,
+      defaults: { ease: "none" },
+      onReverseComplete: () => tl.totalTime(tl.rawTime() + tl.duration() * 100)
+    }),
+    length = items.length,
+    startX = items[0].offsetLeft,
+    times = [],
+    widths = [],
+    xPercents = [],
+    curIndex = 0,
+    pixelsPerSecond = (config.speed || 1) * 100,
+    snap = config.snap === false ? (v) => v : gsap.utils.snap(config.snap || 1), // some browsers shift by a pixel to accommodate flex layouts, so for example if width is 20% the first element's width might be 242px, and the next 243px, alternating back and forth. So we snap to 5 percentage points to make things look more natural
+    totalWidth,
+    curX,
+    distanceToStart,
+    distanceToLoop,
+    item,
+    i;
+  gsap.set(items, {
+    // convert "x" to "xPercent" to make things responsive, and populate the widths/xPercents Arrays to make lookups faster.
+    xPercent: (i, el) => {
+      let w = (widths[i] = parseFloat(gsap.getProperty(el, "width", "px")));
+      xPercents[i] = snap(
+        (parseFloat(gsap.getProperty(el, "x", "px")) / w) * 100 +
+          gsap.getProperty(el, "xPercent")
+      );
+      return xPercents[i];
+    }
+  });
+  gsap.set(items, { x: 0 });
+  totalWidth =
+    items[length - 1].offsetLeft +
+    (xPercents[length - 1] / 100) * widths[length - 1] -
+    startX +
+    items[length - 1].offsetWidth *
+      gsap.getProperty(items[length - 1], "scaleX") +
+    (parseFloat(config.paddingRight) || 0);
+  for (i = 0; i < length; i++) {
+    item = items[i];
+    curX = (xPercents[i] / 100) * widths[i];
+    distanceToStart = item.offsetLeft + curX - startX;
+    distanceToLoop =
+      distanceToStart + widths[i] * gsap.getProperty(item, "scaleX");
+    tl.to(
+      item,
+      {
+        xPercent: snap(((curX - distanceToLoop) / widths[i]) * 100),
+        duration: distanceToLoop / pixelsPerSecond
+      },
+      0
+    )
+      .fromTo(
+        item,
+        {
+          xPercent: snap(
+            ((curX - distanceToLoop + totalWidth) / widths[i]) * 100
+          )
+        },
+        {
+          xPercent: xPercents[i],
+          duration:
+            (curX - distanceToLoop + totalWidth - curX) / pixelsPerSecond,
+          immediateRender: false
+        },
+        distanceToLoop / pixelsPerSecond
+      )
+      .add("label" + i, distanceToStart / pixelsPerSecond);
+    times[i] = distanceToStart / pixelsPerSecond;
+  }
+  function toIndex(index, vars) {
+    vars = vars || {};
+    Math.abs(index - curIndex) > length / 2 &&
+      (index += index > curIndex ? -length : length); // always go in the shortest direction
+    let newIndex = gsap.utils.wrap(0, length, index),
+      time = times[newIndex];
+    if (time > tl.time() !== index > curIndex) {
+      // if we're wrapping the timeline's playhead, make the proper adjustments
+      vars.modifiers = { time: gsap.utils.wrap(0, tl.duration()) };
+      time += tl.duration() * (index > curIndex ? 1 : -1);
+    }
+    curIndex = newIndex;
+    vars.overwrite = true;
+    return tl.tweenTo(time, vars);
+  }
+  tl.next = (vars) => toIndex(curIndex + 1, vars);
+  tl.previous = (vars) => toIndex(curIndex - 1, vars);
+  tl.current = () => curIndex;
+  tl.toIndex = (index, vars) => toIndex(index, vars);
+  tl.times = times;
+  tl.progress(1, true).progress(0, true); // pre-render for performance
+  if (config.reversed) {
+    tl.vars.onReverseComplete();
+    tl.reverse();
+  }
+  return tl;
+}
+}
+horscrolltext()
 
