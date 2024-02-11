207a70e643e91feeb418fe4178776ad18f62a306
RandomThacker <randomthacker@gmail.com>
2024-02-11 11:29:23 +0530

    added touch for mobile

--- js/script.js
+++ js/script.js
@@ -39,7 +39,7 @@ function loaderAnimation(){
   var loader = document.querySelector("#loader")
      setTimeout(function(){
      loader.style.top = "-100%"
-     },4000)
+     },1000)
  }
  loaderAnimation()
 
